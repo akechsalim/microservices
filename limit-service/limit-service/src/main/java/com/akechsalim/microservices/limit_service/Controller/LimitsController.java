60df7c5de86d14adb44a984cd1cd24950f58b7f2
akechsalim <akechsalimwork@gmail.com>
2024-10-01 13:39:16 +0300

    addition of spring cloud config server

--- limit-service/limit-service/src/main/java/com/akechsalim/microservices/limit_service/Controller/LimitsController.java
+++ limit-service/limit-service/src/main/java/com/akechsalim/microservices/limit_service/Controller/LimitsController.java
@@ -1,14 +1,21 @@
 package com.akechsalim.microservices.limit_service.Controller;
 
 import com.akechsalim.microservices.limit_service.Bean.Limits;
+import com.akechsalim.microservices.limit_service.configuraton.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsController {
 
+    private final Configuration configuration;
+
+    public LimitsController(Configuration configuration) {
+        this.configuration = configuration;
+    }
+
     @GetMapping("/limits")
-    public Limits retrieveLimits(){
-        return new Limits(1,1000);
+    public Limits retrieveLimits() {
+        return new Limits(configuration.getMinimum(), configuration.getMaximum());
     }
 }
