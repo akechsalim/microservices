b70e130578415fa00fc8dfe67fcc709593252ac5
akechsalim <akechsalimwork@gmail.com>
2024-10-07 20:31:18 +0300

    addition of currency exchange microservice

---
+++ currency-exchange-service/currency-exchange-service/src/test/java/com/akechsalim/microservices/currency_exchange_service/CurrencyExchangeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akechsalim.microservices.currency_exchange_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
